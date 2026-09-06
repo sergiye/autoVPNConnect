91375fed673b7a6aa0509aa8f14a76e374091656
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2026-09-06 21:08:00 +0300

    auto-build fix

--- AutoVPNConnect/Program.cs
+++ AutoVPNConnect/Program.cs
@@ -15,7 +15,7 @@ namespace AutoVPNConnect {
       Crasher.Listen();
       // SetCurrentProcessExplicitAppUserModelID(Updater.ApplicationCompany + "." + Updater.ApplicationName);
 
-      if (!OSHelper.IsCompatible(true, out var errorMessage, out var fixAction)) {
+      if (!OSHelper.IsCompatible(true, out var errorMessage, out var fixAction, false)) {
         if (fixAction != null) {
           if (MessageBox.Show(errorMessage, Updater.ApplicationTitle, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
             fixAction?.Invoke();
